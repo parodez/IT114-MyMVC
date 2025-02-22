95dbcc3183b1e15f18a004d19ec9f652ea6765ed
parodez <166378157+parodez@users.noreply.github.com>
2025-02-22 10:30:17 +0800

    Add project files.

---
+++ MyMVC/Models/MyData.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMVC.Models
+{
+    public class MyData
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+    }
+}
